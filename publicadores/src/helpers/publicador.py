3b36b709c6f65b7e4fde697cb8c93de50e2b40e5
VLunaa <vusilva.luna@gmail.com>
2023-05-13 17:33:51 -0600

    Cambio publicador.py

--- publicadores/src/helpers/publicador.py
+++ publicadores/src/helpers/publicador.py
@@ -23,29 +23,10 @@
 #           |                        |    será enviado          |                       |
 #           +------------------------+--------------------------+-----------------------+
 #
-<<<<<<< HEAD
-#-------------------------------------------------------------------------
-=======
-# -------------------------------------------------------------------------
-# import pika
-
-# def publish(queue, data):
-#     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-#     channel = connection.channel()
-#     channel.queue_declare(queue=queue, durable=True)
-#     channel.basic_publish(exchange='', routing_key=queue, body=data, properties=pika.BasicProperties(delivery_mode=2))
-#     connection.close()
-
->>>>>>> 1b3f3cc542394103452e695f468ef6fecd2979d8
 import stomp
-
 
 def publish(queue, data):
     conn = stomp.Connection()
     conn.connect('localhost', 61613, wait=True)
     conn.send(queue, data, persistent='true')
-<<<<<<< HEAD
     conn.disconnect()
-=======
-    conn.disconnect()
->>>>>>> 1b3f3cc542394103452e695f468ef6fecd2979d8
